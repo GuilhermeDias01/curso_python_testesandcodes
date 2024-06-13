27ebd972b1e89ca974ec61cac809effaae97058a
GuilhermeDias01 <guilhermediasgd659@gmail.com>
2024-06-13 12:38:20 -0300

    Adicionando Aula 4 e 5

---
+++ cursopy_codigos_iniciais/aula4.py
@@ -0,0 +1,22 @@
+# Tipos int e float:
+# Int > Número inteiro
+# o tipo int representa qualquer numero positivo ou negativo
+
+print(11)  #int
+print(-11) #int 
+print(0)   #int 
+
+# Float > Número com ponto flutuante
+# O tipo Float representa qualquer número positivo ou negativo
+# com ponto flutuante.
+# o . é utulizado para separar as casas decimais
+
+print(1.1) #Float 
+print(1.33) #Float 
+print(-1.32) #Float 
+
+# A função type mostra o tipo que o Python inferiu ao valor
+print(type('Lucas'))
+print(type(1))
+print(type(-1.1))
+print(type(-1.1),type(2))
---
+++ cursopy_codigos_iniciais/aula5.py
@@ -0,0 +1,7 @@
+# TIpo de dado Bool (Boolean)
+# AO questionar algo em um programa so existem duas reposta possíveis:
+# Sim(True) e Não (False)
+# Existem vários operadores para "questionar"
+# Dentre eles o ==, que é um operador lógico 
+# que questiona e o valor é igual a outro
+
