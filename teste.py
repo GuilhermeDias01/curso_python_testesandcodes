822e6feee37f714640dfbe0fcaa0e7e54f257e0a
GuilhermeDias01 <guilhermediasgd659@gmail.com>
2024-09-16 21:59:29 -0300

    Adicionando novas aulas (114 a 120).

--- teste.py
+++ teste.py
@@ -1,48 +1,17 @@
-"""
-Exercício
-Crie uma função que encontra o primeiro duplicado considerando o segundo
-número como a duplicação. Retorne a duplicação considerada.
-Requisitos:
-    A ordem do número duplicado é considerada a partir da segunda
-    ocorrência do número, ou seja, o número duplicado em si.
-    Exemplo:
-        [1, 2, 3, ->3<-, 2, 1] -> 1, 2 e 3 são duplicados (retorne 3)
-        [1, 2, 3, 4, 5, 6] -> Retorne -1 (não tem duplicados)
-        [1, 4, 9, 8, ->9<-, 4, 8] (retorne 9)
-    Se não encontrar duplicados na lista, retorne -1
-"""
-lista_de_listas_de_inteiros = [
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],
-    [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],
-    [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],
-    [4, 8, 8, 8, 5, 1, 10, 3, 1, 7],
-    [1, 3, 7, 2, 2, 1, 5, 1, 9, 9],
-    [10, 2, 2, 1, 3, 5, 10, 5, 10, 1],
-    [1, 6, 1, 5, 1, 1, 1, 4, 7, 3],
-    [1, 3, 7, 1, 10, 5, 9, 2, 5, 7],
-    [4, 7, 6, 5, 2, 9, 2, 1, 2, 1],
-    [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
-    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
-]
+def soma (x, y):
+    return x + y
+
+def chama_funcao(func, x):
+    def interna(y):
+        return func(x,y)
+    return interna
+
+soma_10 = chama_funcao(soma,10)
+print(soma_10(20))
 
 
+def soma_20(y):
+    x = 20
+    return x+y
 
-def verifica_numero_duplicado(lista):
-    lista_de_verificacao = set()
-    duplicado = -1
-
-    for numero in lista:
-        
-        if numero in lista_de_verificacao:
-            duplicado = numero
-            break
-
-        lista_de_verificacao.add(numero)
-        
-    return print(duplicado)
-
-for lista in lista_de_listas_de_inteiros:
-    verifica_numero_duplicado(lista)
-    
-
+print(soma_20(30))
